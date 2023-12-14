f0826a982438df96be8c32bf8f30f4edb86cbc9c
Aman Sharma <amansharmarj13@gmail.com>
2023-12-15 03:35:42 +0530

    add second API and logic

--- Python/code-file.py
+++ Python/code-file.py
@@ -12,3 +12,14 @@ fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}'
 # Get exchange rate data from Fixer
 exchange_data = get_exchange_rate(fixer_url)
 print(exchange_data)
+
+def compare_exchange_rates(currency_1, currency_2):
+    # Construct API URLs for different currencies
+    fixer_api_key = 'YOUR_FIXER_API_KEY'
+    fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}&symbols={currency_1},{currency_2}'
+
+    currency_beacon_api_key = 'YOUR_CURRENCY_BEACON_API_KEY'
+    currency_beacon_url = f'https://api.currencybeacon.com/v1/latest?api_key={currency_beacon_api_key}&base={currency_1}&symbols={currency_2}'
+ # Get exchange rate data from Fixer and Currency Beacon APIs
+    fixer_data = get_exchange_rate(fixer_url)
+    currency_beacon_data = get_exchange_rate(currency_beacon_url)
