722b4ca47034b3c7a888d511855c617297216720
Aman Sharma <amansharmarj13@gmail.com>
2023-12-15 03:30:45 +0530

    Add API

--- Python/code-file.py
+++ Python/code-file.py
@@ -4,3 +4,11 @@ def get_exchange_rate(api_url):
     response = requests.get(api_url)
     data = response.json() if response.status_code == 200 else None 
     return data
+
+#Define API URL
+fixer_api_key = 'YOUR_FIXER_API_KEY'
+fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}'
+
+# Get exchange rate data from Fixer
+exchange_data = get_exchange_rate(fixer_url)
+print(exchange_data)
