882280172328cc492f856764707431fc822863f6
Aman Sharma <amansharmarj13@gmail.com>
2023-12-15 03:46:13 +0530

    make some changes

--- Python/code-file.py
+++ Python/code-file.py
@@ -1,24 +1,32 @@
 import requests
 
 def get_exchange_rate(api_url):
-    response = requests.get(api_url)
-    data = response.json() if response.status_code == 200 else None 
-    return data
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print("Failed to fetch data from the API.")
+            return None
+    except requests.RequestException as e:
+        print("Request Exception:", e)
+        return None
 
-#Define API URL
-fixer_api_key = 'YOUR_FIXER_API_KEY'
-fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}'
+# #Define API URL
+# fixer_api_key =  '0d08fdee18bfba444988679a06fa7a02'
+# fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}&symbols={currency_1},{currency_2}'
 
-# Get exchange rate data from Fixer
-exchange_data = get_exchange_rate(fixer_url)
-print(exchange_data)
+# # Get exchange rate data from Fixer
+# exchange_data = get_exchange_rate(fixer_url)
+# print(exchange_data)
 
 def compare_exchange_rates(currency_1, currency_2):
     # Construct API URLs for different currencies
-    fixer_api_key = 'YOUR_FIXER_API_KEY'
+    fixer_api_key = '0d08fdee18bfba444988679a06fa7a02'
     fixer_url = f'http://data.fixer.io/api/latest?access_key={fixer_api_key}&symbols={currency_1},{currency_2}'
 
-    currency_beacon_api_key = 'YOUR_CURRENCY_BEACON_API_KEY'
+    currency_beacon_api_key ='vGhiQ81fq6RxhT79HCSNz8nzMCrIvn6R'
     currency_beacon_url = f'https://api.currencybeacon.com/v1/latest?api_key={currency_beacon_api_key}&base={currency_1}&symbols={currency_2}'
 
 # Get exchange rate data from Fixer and Currency Beacon APIs
