47b6fe9783d62648066bb3e1476661989213438e
Aman Sharma <amansharmarj13@gmail.com>
2023-12-15 03:37:51 +0530

    print comparison

--- Python/code-file.py
+++ Python/code-file.py
@@ -20,6 +20,21 @@ def compare_exchange_rates(currency_1, currency_2):
 
     currency_beacon_api_key = 'YOUR_CURRENCY_BEACON_API_KEY'
     currency_beacon_url = f'https://api.currencybeacon.com/v1/latest?api_key={currency_beacon_api_key}&base={currency_1}&symbols={currency_2}'
- # Get exchange rate data from Fixer and Currency Beacon APIs
+
+# Get exchange rate data from Fixer and Currency Beacon APIs
     fixer_data = get_exchange_rate(fixer_url)
     currency_beacon_data = get_exchange_rate(currency_beacon_url)
+
+# Print exchange rates and comparison
+    if fixer_data and currency_beacon_data:
+        print(f"Exchange rate from Fixer for {currency_1} to {currency_2}: {fixer_data['rates'][currency_2]}")
+        print(f"Exchange rate from Currency Beacon for {currency_1} to {currency_2}: {currency_beacon_data['rates'][currency_2]}")
+
+        if fixer_data['rates'][currency_2] < currency_beacon_data['rates'][currency_2]:
+            print("Fixer provides a better exchange rate.")
+        elif fixer_data['rates'][currency_2] > currency_beacon_data['rates'][currency_2]:
+            print("Currency Beacon provides a better exchange rate.")
+        else:
+            print("Both APIs provide the same exchange rate.")
+    else:
+        print("Unable to compare exchange rates.")
